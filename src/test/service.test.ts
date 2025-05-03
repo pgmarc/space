5839b9ba7dd6977745c7d8c712cbbc1713900145
Alex-GF <yalejandro9@gmail.com>
2025-05-03 18:16:05 +0200

    feat: service pricings initial tests

--- src/test/service.test.ts
+++ src/test/service.test.ts
@@ -20,6 +20,62 @@ describe('Get public user information', function () {
       const response = await request(app).get('/api/services');
       expect(response.status).toEqual(200);
       expect(response.body).toBeDefined();
+      expect(Array.isArray(response.body)).toBe(true);
+      expect(response.body.length).toBeGreaterThan(0);
+    });
+  })
+
+  describe('GET /services/{serviceName}', function () {
+    it('Should return 200: Given existent service name in lower case', async function () {
+      const response = await request(app).get('/api/services/zoom');
+      expect(response.status).toEqual(200);
+      expect(Array.isArray(response.body)).toBe(false);
+      expect(response.body.name.toLowerCase()).toBe("zoom");
+    });
+
+    it('Should return 200: Given existent service name in upper case', async function () {
+      const response = await request(app).get('/api/services/ZOOM');
+      expect(response.status).toEqual(200);
+      expect(Array.isArray(response.body)).toBe(false);
+      expect(response.body.name.toLowerCase()).toBe("zoom");
+    });
+
+    it('Should return 404 due to service not found', async function () {
+      const response = await request(app).get('/api/services/unexistent-service');
+      expect(response.status).toEqual(404);
+      expect(response.body.error).toBe("Service unexistent-service not found");
+    });
+  })
+
+  describe('GET /services/{serviceName}/pricings', function () {
+    it('Should return 200: Given existent service name in lower case', async function () {
+      const response = await request(app).get('/api/services/zoom/pricings');
+      expect(response.status).toEqual(200);
+      expect(Array.isArray(response.body)).toBe(true);
+      expect(response.body.length).toBeGreaterThan(0);
+      expect(response.body[0].features).toBeDefined();
+      expect(Object.keys(response.body[0].features).length).toBeGreaterThan(0);
+      expect(response.body[0].usageLimits).toBeDefined();
+      expect(response.body[0].plans).toBeDefined();
+      expect(response.body[0].addOns).toBeDefined();
+    });
+
+    it('Should return 200: Given existent service name in upper case', async function () {
+      const response = await request(app).get('/api/services/ZOOM/pricings');
+      expect(response.status).toEqual(200);
+      expect(Array.isArray(response.body)).toBe(true);
+      expect(response.body.length).toBeGreaterThan(0);
+      expect(response.body[0].features).toBeDefined();
+      expect(Object.keys(response.body[0].features).length).toBeGreaterThan(0);
+      expect(response.body[0].usageLimits).toBeDefined();
+      expect(response.body[0].plans).toBeDefined();
+      expect(response.body[0].addOns).toBeDefined();
+    });
+
+    it('Should return 404 due to service not found', async function () {
+      const response = await request(app).get('/api/services/unexistent-service/pricings');
+      expect(response.status).toEqual(404);
+      expect(response.body.error).toBe("Service unexistent-service not found");
     });
   })
 
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-      include: ['**/*.test.ts'], // Solo incluye los tests del directorio API
+      include: ['**/*.test.ts'], // Incluye solo archivos de prueba
       globals: true, // Habilita la API global de Vitest
       environment: 'node', // Usa el entorno Node.js
       typecheck: {
