03b0a4cedec8231d5d010f02b4f8ea97e2e11161
Alex-GF <yalejandro9@gmail.com>
2025-05-22 13:36:38 +0200

    fix: adpated db connections to new envs

--- src/main/config/mongoose.ts
+++ src/main/config/mongoose.ts
@@ -3,19 +3,44 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 
+// const getMongoDBConnectionURI = () => {
+//   const databaseProtocol = process.env.MONGO_PROTOCOL;
+//   const databaseHost = process.env.MONGO_HOST;
+//   const databasePort = process.env.MONGO_PORT ? `:${process.env.MONGO_PORT}` : ':27017';
+//   const databaseUsername = process.env.DATABASE_USERNAME;
+//   const databasePassword = process.env.DATABASE_PASSWORD;
+//   const databaseName = process.env.DATABASE_NAME;
+//   const dbCredentials =
+//     databaseUsername && databasePassword ? databaseUsername + ':' + databasePassword + '@' : '';
+//   const authSource = databaseProtocol === 'mongodb+srv' ? '' : `?authSource=${databaseName}`;
+//   const mongoDbConnectionURI = `${databaseProtocol}://${dbCredentials}${databaseHost}${databasePort}/${databaseName}${authSource}`;
+//   return mongoDbConnectionURI;
+// };
+
 const getMongoDBConnectionURI = () => {
-  const databaseProtocol = process.env.MONGO_PROTOCOL;
-  const databaseHost = process.env.MONGO_HOST;
-  const databasePort = process.env.MONGO_PORT ? `:${process.env.MONGO_PORT}` : '';
-  const databaseUsername = process.env.DATABASE_USERNAME;
-  const databasePassword = process.env.DATABASE_PASSWORD;
   const databaseName = process.env.DATABASE_NAME;
+  
+  if (!databaseName && !process.env.MONGO_URI) {
+    throw new Error('You need to provide a database name through the DATABASE_NAME environment variable');
+  }
+
+  const databaseUsername = process.env.DATABASE_USERNAME;
+
+  if (!databaseUsername && !process.env.MONGO_URI) {
+    throw new Error('You need to provide a database username through the DATABASE_USERNAME environment variable');
+  }
+
+  const databasePassword = process.env.DATABASE_PASSWORD;
+
+  if (!databasePassword && !process.env.MONGO_URI) {
+    throw new Error('You need to provide a database password through the DATABASE_PASSWORD environment variable');
+  }
+
   const dbCredentials =
     databaseUsername && databasePassword ? databaseUsername + ':' + databasePassword + '@' : '';
-  const authSource = databaseProtocol === 'mongodb+srv' ? '' : `?authSource=${databaseName}`;
-  const mongoDbConnectionURI = `${databaseProtocol}://${dbCredentials}${databaseHost}${databasePort}/${databaseName}${authSource}`;
-  return mongoDbConnectionURI;
-};
+
+  return process.env.MONGO_URI || `mongodb://${dbCredentials}localhost:27017/${databaseName}`;
+}
 
 const initMongoose = () => {
   const mongoDbConnectionURI = getMongoDBConnectionURI();
