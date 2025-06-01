dc684dc08f2bf451d93721db7d294c8e10e78b7e
Alex-GF <yalejandro9@gmail.com>
2025-06-01 10:22:53 +0200

    feat: enabled cors in server

--- api/src/main/middlewares/GlobalMiddlewaresLoader.ts
+++ api/src/main/middlewares/GlobalMiddlewaresLoader.ts
@@ -5,7 +5,12 @@ import { apiKeyAuthMiddleware } from './ApiKeyAuthMiddleware';
 
 const loadGlobalMiddlewares = (app: express.Application) => {
   app.use(express.json());
-  app.use(cors());
+  app.use(cors({
+    origin: process.env.ENVIRONMENT === "development" ? '*' : "http://localhost:5403", // Allow all origins, adjust as necessary for your security needs
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+    allowedHeaders: ['Content-Type', 'x-api-key'], // Specify allowed headers
+    credentials: true // Allow credentials if needed
+  }));
   app.use(helmet(
     {
       crossOriginResourcePolicy: false // allows loading of files from /public
